5b0a0559af02af4e942078a63de7b0b79d6040ec
ianno_000 <ianno_000@ian-pc.home>
2014-07-27 13:33:24 -0400

    yes

--- my-gdx-game-core/src/com/mygdx/game/MyGdxGame.java
+++ my-gdx-game-core/src/com/mygdx/game/MyGdxGame.java
@@ -19,7 +19,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
 		b = new Board(0, 0, 10, 10, 20);
 		artist = new HexagonBoardRenderer(b);
 		
@@ -30,8 +30,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		//batch.draw(img, 0, 0);
 		artist.drawBoard();
 		batch.end();
 	}
-}
+}jkl

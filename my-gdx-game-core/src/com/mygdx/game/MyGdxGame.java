a5ab1dacbd383ab944d210601774d214cbb48f22
jagorski2 <gorskijim@gmail.com>
2014-08-01 15:55:33 -0400

    Should be working well

--- my-gdx-game-core/src/com/mygdx/game/MyGdxGame.java
+++ my-gdx-game-core/src/com/mygdx/game/MyGdxGame.java
@@ -6,12 +6,11 @@ import java.util.List;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.graphics.GL20;
-
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.PolygonRegion;
 import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
-
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
@@ -37,6 +36,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	
 	@Override
 	public void create () {
+		Preferences prefs = Gdx.app.getPreferences("settings.prefs");
 		sprite_batch = new PolygonSpriteBatch(50);
 		textureGreen = new TextureRegion(new Texture(Gdx.files.internal("textures/Green.png")),800,800);
 		textureGrass = new TextureRegion(new Texture(Gdx.files.internal("textures/Grass.jpg")),800,800);
@@ -68,7 +68,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-
+		
 		battle.drawBattleInstance();
 		
 
